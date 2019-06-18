87835a3e1625842a950356d7f350cc790fde94c9
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 23:32:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,13 +3,6 @@ class Student
   attr_accessor :name, :grade, :id
 
 
-  def initialize (name, grade, id = nil)
-    @name = name 
-    @grade = grade 
-    @id = id
-  end 
-
-
   def self.create_table
       sql =  <<-SQL 
        CREATE TABLE IF NOT EXISTS students (
