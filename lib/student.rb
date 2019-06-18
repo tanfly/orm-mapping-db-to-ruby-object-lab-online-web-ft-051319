c1f7e782453a72e2e8db12f86a659ba9ea46da11
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 23:44:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,7 +98,7 @@ class Student
         LIMIT ?
         SQL
       
-        DB[:conn].execute(sql).map do |row| 
+        DB[:conn].execute(sql, num_of_students).map do |row| 
           self.new_from_db(row)
         end 
       end 
