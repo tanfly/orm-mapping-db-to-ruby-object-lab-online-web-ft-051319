7543591966687e186a2b1ba97c9bec70e815fae4
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 23:30:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,18 +68,6 @@ end
   
  
 
-  def self.all
-    sql = <<-SQL
-    SELECT * 
-    FROM students
-    SQL 
-  
-  DB[:conn].execute(sql).map do |row| 
-  self.new_from_db(row)
-  end
-  end
-end
-
   
   
 end
