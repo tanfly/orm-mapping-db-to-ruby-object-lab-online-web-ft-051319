386d9cfa6eee5650fb344eb72ce072ad4fdabf92
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 23:36:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,19 +66,19 @@ class Student
 12.   end
 13. end
 
-def self.find_by_name(name)
-  sql = <<-SQL 
-  SELECT *
-  FROM students 
-  WHERE name = ? 
-  LIMIT 1 
-  SQL
-
-  DB[:conn].execute(sql, name).map do |row| 
-    self.new_from_db(row)
-  end.first 
-end 
-end
+  def self.find_by_name(name)
+    sql = <<-SQL 
+    SELECT *
+    FROM students 
+    WHERE name = ? 
+    LIMIT 1 
+    SQL
+  
+    DB[:conn].execute(sql, name).map do |row| 
+      self.new_from_db(row)
+    end.first 
+  end 
+  end
  
 
   
