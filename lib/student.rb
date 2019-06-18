48f57783183aa0e1c86ae9c2c02c9fe3391024fc
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 23:24:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :name, :grade, :id
 
 
-  def initialize (name, grade, id = nil)
+  def initialize (name = nil, grade, id = nil)
     @name = name 
     @grade = grade 
     @id = id
