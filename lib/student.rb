52efe0247670003baa958aa60a3a4e71ed0f30bc
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 23:47:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -123,7 +123,7 @@ class Student
         WHERE grade = ? 
         SQL
       
-        DB[:conn].execute(sql, num_of_students).map do |row| 
+        DB[:conn].execute(sql, grade).map do |row| 
           self.new_from_db(row)
         end 
       end 
