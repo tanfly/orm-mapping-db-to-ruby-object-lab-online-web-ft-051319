f9c7d2e67ca570933cf7fc45df024ade947457d1
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 23:42:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,7 +91,7 @@ class Student
       end 
       
       def self.first_X_students_in_grade_10(num_of_students) 
-            sql = <<-SQL 
+        sql = <<-SQL 
         SELECT *
         FROM students 
         WHERE grade = 9 
