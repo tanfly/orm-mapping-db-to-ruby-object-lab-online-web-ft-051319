5c51d81dccc71965055a76cf80cf0be24cb39ba0
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 23:41:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,7 +90,7 @@ class Student
         end 
       end 
       
-      def self.first_X_students_in_grade_10 
+      def self.first_X_students_in_grade_10(num_of_students) 
         
       end
   
