94abac245cf742d6428a484fe144acdb3c929edf
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 23:47:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -116,7 +116,7 @@ class Student
         end.first 
       end 
       
-      def self.all_students_in_grade_x(grade)
+      def self.all_students_in_grade_X(grade)
         sql = <<-SQL 
         SELECT *
         FROM students 
